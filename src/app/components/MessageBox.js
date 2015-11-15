4b091b145e1fd16ffd2fdfafd82ec887912342ce
p-janik <pawel.janik@rspective.pl>
2015-11-15 15:56:52 +0100

    Restyle message box

--- src/app/components/MessageBox.js
+++ src/app/components/MessageBox.js
@@ -10,7 +10,16 @@ class MessageBox extends React.Component {
 
     return (
       <Card style={{maxWidth: 1200, padding: 30, margin: '30px auto'}}>
-        <textarea style={{width: '100%'}}/>
+        <textarea style={{
+        width: '100%',
+        borderColor: '#D0D0D0',
+        resize: 'none',
+        borderRadius: 3,
+        minHeight: 50,
+        color: '#555',
+        fontSize: 15,
+        outline: 'auto 0px'
+        }}/>
       </Card>
     )
   }
